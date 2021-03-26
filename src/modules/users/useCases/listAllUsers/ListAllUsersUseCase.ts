cdfc818093583da70b06da6f976891e53f680728
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-03-26 10:00:15 -0300

    Desafio concluido com sucesso!

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string,
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id)
+
+    if(!user) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if(user.admin === false) {
+      throw new Error("Mensagem do erro");
+    }
+
+    const allUsers = this.usersRepository.list();
+
+    return allUsers;
   }
 }
 
